ce1f663931f82e78695b17a8e582fc1b8fb4b914
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2018-09-19 11:15:47 -0400

    Change Title of App

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'shopping-list-app'`, async(() => {
+  it(`should have as title 'Shopping List App'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('shopping-list-app');
+    expect(app.title).toEqual('Shopping List App');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to shopping-list-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Shopping List App');
   }));
 });
